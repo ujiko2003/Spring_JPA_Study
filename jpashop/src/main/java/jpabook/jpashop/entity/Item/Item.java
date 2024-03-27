c4d331c49cf6344f79357666776e0655aa331838
HyeonJongKim <ujiko2003@bpsoft.co.kr>
2024-03-27 11:46:44 +0900

    상품 엔티티 개발, 상품 리포지토리 개발

--- jpashop/src/main/java/jpabook/jpashop/entity/Item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/entity/Item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.entity.Item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.entity.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,4 +29,22 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    // stockQuantity를 변경하는 비즈니스 로직을 엔티티 안에 넣는다.
+    // 응집력을 높이기 위해 비즈니스 로직을 엔티티에 넣는다.
+    /*
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
